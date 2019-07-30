241339247e97ec62319e3dcb560a7f5613d0b94f
Melinda Golden <info@melindagolden.com>
2019-07-29 20:31:14 -0400

    Added header

--- App.js
+++ App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { StyleSheet, Text, View, ActivityIndicator, FlatList, Image } from 'react-native';
+import * as Font from 'expo-font';
 
 export default class App extends Component {
   // Data for the app
@@ -10,15 +11,21 @@ export default class App extends Component {
 
   // Runs immediately after the component mounts
   componentDidMount() {
+    // Load fonts
+    Font.loadAsync({
+      'lobster-bold': require('./assets/fonts/LobsterTwo-Regular.ttf'),
+    });
+
+    // Dog API URL
     const url = "https://dog.ceo/api/breeds/image/random/3";
 
     // Get the images from the API
     fetch(url)
       .then((res) => res.json())
-      .then ((resJSON) => {
+      .then (({ message }) => {
         this.setState({
           loading: false,
-          images: resJSON.message,
+          images: message,
         });
       })
       .catch((error) => console.error(error));
@@ -28,29 +35,30 @@ export default class App extends Component {
   render() {
     const { loading, images } = this.state;
 
+    // If loading, return loading indicator
     if (loading) {
       return(
-        <View style={{flex: 1, padding: 20}}>
-          <ActivityIndicator/>
+        <View style={styles.container}>
+          <ActivityIndicator
+            size="large"
+            color="#777777"
+          />
         </View>
       )
     }
 
+    // Render images
     return (
       <View style={styles.container}>
+        <View style={styles.header}>
+          <Text style={styles.headerText}>Puppergram</Text>
+        </View>
         <FlatList
-          style={{
-            marginTop: 40,
-          }}
           data={images}
           renderItem={({ item }) => (
             <Image
-              style={{
-                alignSelf: 'center',
-                height: 400,
-                width: 400,
-                marginBottom: 5,
-              }}
+              style={styles.image}
+              resizeMode="cover"
               source={{ uri: item }}
             />
           )}
@@ -65,7 +73,23 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
+    alignItems: 'stretch',
     justifyContent: 'center',
+  },
+  header: {
+    backgroundColor: '#f6f6f6',
+    height: 90,
+    alignItems: 'center',
+    paddingTop: 45,
+  },
+  headerText: {
+    fontFamily: 'lobster-bold',
+    fontSize: 28,
+  },
+  image: {
+    alignSelf: 'center',
+    height: 400,
+    width: 415,
+    marginBottom: 5,
   },
 });
